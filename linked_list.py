9b4cc232aaf3e8a8826595cd72b91b3813a8061a
rudrap-b1987 <96695129+rudrap-b1987@users.noreply.github.com>
2023-07-03 11:17:04 +0530

    Add files via upload

---
+++ linked_list.py
@@ -0,0 +1,26 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert_at_beginning(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def print_list(self):
+        current_node = self.head
+        while current_node is not None:
+            print(current_node.data)
+            current_node = current_node.next
+
+if __name__ == "__main__":
+    list = LinkedList()
+    list.insert_at_beginning(10)
+    list.insert_at_beginning(20)
+    list.insert_at_beginning(30)
+    list.print_list()
